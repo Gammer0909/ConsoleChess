2af320812042469cb7f234b15bf34a129a75349f
Gammer0909 <103458991+Gammer0909@users.noreply.github.com>
2023-10-12 21:51:34 +0000

    Getting started

---
+++ source/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Chess {
+
+
+    char[][] boardASCII = {
+        new char[] {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+        new char[] {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+        new char[] {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+    };
+
+
+    char[][] boardUnicode = {
+        new char[] {'♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'},
+        new char[] {'♟', '♟', '♟', '♟', '♟', '♟', '♟', '♟'},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        new char[] {'♙', '♙', '♙', '♙', '♙', '♙', '♙', '♙'},
+        new char[] {'♖', '♘', '♗', '♕', '♔', '♗', '♘', '♖'}
+    };
+    
+    private bool useUnicode = true;
+    private bool isWhiteTurn = true;
+    
+
+}
